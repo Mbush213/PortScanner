f9cd93657d7940c1c917808deba68a64d4d006bc
Mark H. B. <mmbush2010@gmail.com>
2026-05-16 09:44:19 -0600

    PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -6,3 +6,20 @@ ip_add_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$)")
 port_range_pattern = re.compile("([0-9]+)-([0-9]+)")
 port_min = 0
 port_max = 65535
+
+open_ports = []
+
+while True:
+    ip_add_entered = input("Please enter your IP address: ")
+    if ip_add_pattern.search(ip_add_entered):
+        print(f"{ip_add_entered} is a valid IP address")
+        break
+
+while True:
+    print("Please enter the range of ports you want to scan in format: <int>-<int> (ex would be 60-120)")
+    port_range = input("Enter port range: ")
+    port_range_valid = port_range_pattern.search(port_range.replace(" ", ""))
+    if port_range_valid:
+        port_min = int(port_range_valid.group(1))
+        port_max = int(port_range_valid.group(2))
+        break
